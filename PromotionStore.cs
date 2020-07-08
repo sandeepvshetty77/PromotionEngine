91ef1472cdf5a7798e2cfd2bbdf3e65d7576df30
Sandeep Shetty <sandeepvs@yahoo.com>
2020-07-08 11:32:56 +0100

    Added PromotionStore which will hold all the promotions.

---
+++ PromotionStore.cs
@@ -0,0 +1,25 @@
+﻿using PromotionEngineNS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionEngineNS
+{
+    public static class PromotionStore
+    {
+        // List of different promotions as objects
+        static List<IPromotion> promotionList = new List<IPromotion>();
+
+        public static void AddPromotion(IPromotion promotion)
+        {
+            promotionList.Add(promotion);
+        }
+
+        public static List<IPromotion> GetAllPromotions()
+        {
+            return promotionList;
+        }
+    }
+}
