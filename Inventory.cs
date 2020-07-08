24fbb53f45fd94a366144092ba9b4774c679cd40
Sandeep Shetty <sandeepvs@yahoo.com>
2020-07-08 10:23:47 +0100

    Adding SKU and Inventory class. Inventory class is static which is place folder for a database which will store all the SKUs

---
+++ Inventory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionEngineNS
+{
+    public static class Inventory
+    {
+        private static List<SKU> _itemsInStore = null;
+
+        static Inventory()
+        {
+            _itemsInStore = new List<SKU>();
+        }
+    }
+}
---
+++ SKU.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionEngineNS
+{
+    public class SKU
+    {
+        public SKU(char id, int price)
+        {
+            Id = id;
+            Price = price;
+        }
+        public char Id { get;  }
+        public int Price { get; }
+    }
+}
